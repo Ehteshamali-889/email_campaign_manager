2be30dc1d20a5a7896ba64f51f7526695b41c46f
Ehteshamali-889 <webdeveloper3628@gmail.com>
2025-05-04 23:56:00 +0500

    fixes

--- src/EmailCampaignManagerServiceProvider.php
+++ src/EmailCampaignManagerServiceProvider.php
@@ -18,27 +18,27 @@ class EmailCampaignManagerServiceProvider extends ServiceProvider
     {
         // Publish migrations
         $this->publishes([
-            __DIR__.'/../database/migrations' => database_path('migrations'),
+            __DIR__ . '/../database/migrations' => database_path('migrations'),
         ], 'migrations');
 
-        // Publish models (Optional - useful if you want to customize models)
+        // Publish models
         $this->publishes([
-            __DIR__.'/../Models' => app_path('Models'),
+            __DIR__ . '/../Models' => app_path('Models'),
         ], 'models');
 
         // Publish views
         $this->publishes([
-            __DIR__.'/../views/emails' => resource_path('views/vendor/email-campaign-manager'),
+            __DIR__ . '/../views/emails' => resource_path('views/vendor/email-campaign-manager'),
         ], 'views');
 
         // Publish routes
         $this->publishes([
-            __DIR__.'/../routes/api.php' => base_path('routes/api.php'),
+            __DIR__ . '/../routes/api.php' => base_path('routes/api.php'),
         ], 'routes');
 
-        // Optionally, publish config file if you plan to add configuration options later
+        // Optionally, publish config file
         $this->publishes([
-            __DIR__.'/../config/email_campaign_manager.php' => config_path('email_campaign_manager.php'),
+            __DIR__ . '/../config/email_campaign_manager.php' => config_path('email_campaign_manager.php'),
         ], 'config');
     }
 }
